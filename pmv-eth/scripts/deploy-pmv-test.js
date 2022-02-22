37c6386aaf8fe5f1943b77999544caad01ef1d17
freddyaboulton <alfonsoboulton@gmail.com>
2022-02-22 18:17:35 -0500

    Modify update script

--- pmv-eth/scripts/deploy-pmv-test.js
+++ pmv-eth/scripts/deploy-pmv-test.js
@@ -36,6 +36,16 @@ async function main() {
 
   const [deployer] = await ethers.getSigners();
 
+  const linkToken = '0x01BE23585060835E02B77ef475b0Cc51aA1e0709';
+  const vrfCoordinator = '0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B';
+  const keyHash = 
+    '0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311';
+  const linkFee = ethers.BigNumber.from('100000000000000000');
+  
+  // dummy provenance hash
+  const provenanceHash = 
+    '0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc';
+
   // Construct merkle tree here for WHITELIST
   const merkleEntries = [['0xE4763c199bdD01aa01c5dc4e9524c63F307e9021', 2],
   ['0x7b1C4134a8682dbee5AF7993DEc9745e11263E8f', 20],
@@ -55,10 +65,11 @@ async function main() {
   // need to add freeRoot if different
   const pmv = await PMV.connect(deployer).deploy(root,
     'https://my-json-server.typicode.com/freddyaboulton/pmv-token/tokens/',
-    root);
+    root, provenanceHash, vrfCoordinator, linkToken, keyHash,
+    linkFee);
   const pmvOpt = await PMVOptimized.connect(deployer).deploy(root,
     'https://my-json-server.typicode.com/freddyaboulton/pmv-token/tokens/',
-    root);
+    root, provenanceHash, vrfCoordinator, linkToken, keyHash, linkFee);
 
   console.log('PMV deployed to:', pmv.address);
   console.log('PMVOptimized deployed to:', pmvOpt.address);
