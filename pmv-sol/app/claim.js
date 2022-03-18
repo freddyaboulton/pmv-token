920a4e10becd07a405051be9f4c2c44993265b83
freddyaboulton <alfonsoboulton@gmail.com>
2022-03-17 22:03:40 -0400

    First draft

--- pmv-sol/app/claim.js
+++ pmv-sol/app/claim.js
@@ -10,6 +10,7 @@ import {MintLayout, Token} from '@solana/spl-token';
 import {AUTHORITY, CONFIG, UUID,
   TOKEN_PROGRAM_ID, TOKEN_METADATA_PROGRAM_ID} from './constants.js';
 import {SystemProgram} from '@solana/web3.js';
+import { Metadata } from '@metaplex-foundation/mpl-token-metadata';
 
 
 /**
@@ -115,4 +116,30 @@ export async function isClaimed(tokenIndex) {
   } else {
     return status;
   }
+}
+
+const createdByOurCandyMachine = (data, candyMachineAddress) => {
+  if (data.creators == null) {
+    return false;
+  } else if(data.creators.length == 0) {
+    return false;
+  }
+  else {
+    return data.creators[0].address == candyMachineAddress;
+  }
+}
+
+/**
+ * Get all POMV NFTS owned by an address
+ * @param {str} publicKey - Address
+ * @return {Array} list of token addresses owned by this address
+ */
+export async function getTokensOfOwner(publicKey) {
+  const allNFTs = await Metadata.findDataByOwner(provider.connection, publicKey);
+  const [candyMachine] = await getCandyMachine(
+    CONFIG,
+    UUID,
+);
+  // Our candy machine is the first creator
+  return allNFTs.filter(token => createdByOurCandyMachine(token, candyMachine.toString()));
 }
